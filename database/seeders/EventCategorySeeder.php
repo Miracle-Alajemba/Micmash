b5c195e2521f0aef0897bd4cc9182fe50bb983aa
miraclealajemba <miraclealajemba@gmail.com>
2026-01-03 13:50:05 +0100

    updated the event cateogry seeder

--- database/seeders/EventCategorySeeder.php
+++ database/seeders/EventCategorySeeder.php
@@ -7,21 +7,22 @@ use App\Models\EventCategory;
 
 class EventCategorySeeder extends Seeder
 {
-  public function run(): void
-  {
-    $categories = [
-      'Technology',
-      'Music & Arts',
-      'Sports & Fitness',
-      'Business & Career',
-      'Food & Drink',
-      'Charity & Causes',
-      'Community & Culture',
-      'Family & Education'
-    ];
+    public function run(): void
+    {
+        $categories = [
+            'Technology',
+            'Music & Arts',
+            'Sports & Fitness',
+            'Business & Career',
+            'Food & Drink',
+            'Charity & Causes',
+            'Community & Culture',
+            'Family & Education',
+            'conferences'
+        ];
 
-    foreach ($categories as $category) {
-      EventCategory::create(['name' => $category]);
+        foreach ($categories as $category) {
+            EventCategory::create(['name' => $category]);
+        }
     }
-  }
 }
