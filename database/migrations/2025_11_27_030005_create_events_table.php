bb6b785133f295daf5e3a1c84083c52a5b2c479f
miraclealajemba <miraclealajemba@gmail.com>
2026-03-01 04:21:38 +0100

    Fixed a little details

--- database/migrations/2025_11_27_030005_create_events_table.php
+++ database/migrations/2025_11_27_030005_create_events_table.php
@@ -14,9 +14,7 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Organizer
-
             $table->foreignId('category_id')->constrained('event_categories')->onDelete('cascade');
-
             $table->string('title');
             $table->text('description');
             $table->string('location');
